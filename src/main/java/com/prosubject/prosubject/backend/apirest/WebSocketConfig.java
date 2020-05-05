597a98e9915c6c514efac6ff6205f5a0cf7b0327
Raul <raulrodriguezmendez1997@gmail.com>
2020-05-05 22:04:32 +0200

    Cambiada url del foro

--- src/main/java/com/prosubject/prosubject/backend/apirest/WebSocketConfig.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
-    	 registry.addEndpoint("/ws").setAllowedOrigins("https://prosubject-v2.herokuapp.com").withSockJS();
+    	 registry.addEndpoint("/ws").setAllowedOrigins("https://prosubject-lanzamiento.herokuapp.com").withSockJS();
     }
 }
